9f42c55dfee949cb7bb9a171f37c78675b5c479a
jasongama <jasongama967@gmail.com>
2019-05-14 17:38:02 +0200

    done

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -1,12 +1,12 @@
 describe('findItemsOver' , function(){
-    it('return all the products that have a quantity higher than 25' , function(){
+    it(' which person is older than 25' , function(){
         assert.deepEqual(findItemsOver([
-            {name : 'apples', qty : 10},
-            {name : 'pears', qty : 37}],25), [ { name: 'pears', qty: 37 } ])
+            {name : 'John', qty : 10},
+            {name : 'Peter', qty : 37}],25), [ { name: 'Peter', qty: 37 } ])
     });
-    it('return all the products that have a quantity higher than 40' , function(){
+    it('which person is older than 40' , function(){
         assert.deepEqual(findItemsOver([
-            {name : 'apples', qty : 10},
-            {name : 'pears', qty : 37}],25), [ { name: 'pears', qty: 37 } ])
+            {name : 'May', qty : 10},
+            {name : 'James', qty : 37}],25), [ { name: 'Jemes', qty: 37 } ])
     });
 });
