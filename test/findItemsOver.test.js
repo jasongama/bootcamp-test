5183b30ab8aec80d9757de4f01fbf7da72ca275a
jasongama <jasongama967@gmail.com>
2019-05-16 10:32:10 +0200

    done

--- test/findItemsOver.test.js
+++ test/findItemsOver.test.js
@@ -1,10 +1,10 @@
 describe('findItemsOver' , function(){
-    it(' which car is over 20 years' , function(){
+    it(' which cars is over 20 years' , function(){
         assert.deepEqual(findItemsOver([
             {name : 'BMW', age : 10},
             {name : 'Mercedes', age : 37}],20), [ { name: 'Peter', age: 37 } ])
     });
-    it('which car is over 25 years' , function(){
+    it('which cars is over 25 years' , function(){
         assert.deepEqual(findItemsOver([
             {name : 'BMW', age : 10},
             {name : 'Mercedes', age : 37}],25), [ { name: 'James', age: 37 } ])
